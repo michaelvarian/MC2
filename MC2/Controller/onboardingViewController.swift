40f5076bc71503baccc5131e7e59375123df4184
Michael Varian Sutanto <michaelvarian.id@gmail.com>
2019-07-18 13:39:19 +0700

    Preparation for today collab. Scene Onboarding sedang di non-aktifkan via AppDelegate

--- MC2/Controller/onboardingViewController.swift
+++ MC2/Controller/onboardingViewController.swift
@@ -20,6 +20,9 @@ class onboardingViewController: UIViewController, UIScrollViewDelegate {
     //create page control outlet
     @IBOutlet weak var pageControl: UIPageControl!
     
+    @IBOutlet weak var pushButton: UIButton!
+    
+    
     //create new variable called slides
     var slides : [Slide] = [];
     
@@ -118,41 +121,4 @@ class onboardingViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
-    func scrollView(_ scrollView: UIScrollView, didScrollToPercentageOffset percentageHorizontalOffset: CGFloat) {
-        if(pageControl.currentPage == 0) {
-            //Change background color to toRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1
-            //Change pageControl selected color to toRed: 103/255, toGreen: 58/255, toBlue: 183/255, fromAlpha: 0.2
-            //Change pageControl unselected color to toRed: 255/255, toGreen: 255/255, toBlue: 255/255, fromAlpha: 1
-            
-            let pageUnselectedColor: UIColor = fade(fromRed: 255/255, fromGreen: 255/255, fromBlue: 255/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
-            pageControl.pageIndicatorTintColor = pageUnselectedColor
-            
-            
-            let bgColor: UIColor = fade(fromRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1, toRed: 255/255, toGreen: 255/255, toBlue: 255/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
-            slides[pageControl.currentPage].backgroundColor = bgColor
-            
-            let pageSelectedColor: UIColor = fade(fromRed: 81/255, fromGreen: 36/255, fromBlue: 152/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
-            pageControl.currentPageIndicatorTintColor = pageSelectedColor
-        }
-    }
-    
-    
-    func fade(fromRed: CGFloat,
-              fromGreen: CGFloat,
-              fromBlue: CGFloat,
-              fromAlpha: CGFloat,
-              toRed: CGFloat,
-              toGreen: CGFloat,
-              toBlue: CGFloat,
-              toAlpha: CGFloat,
-              withPercentage percentage: CGFloat) -> UIColor {
-        
-        let red: CGFloat = (toRed - fromRed) * percentage + fromRed
-        let green: CGFloat = (toGreen - fromGreen) * percentage + fromGreen
-        let blue: CGFloat = (toBlue - fromBlue) * percentage + fromBlue
-        let alpha: CGFloat = (toAlpha - fromAlpha) * percentage + fromAlpha
-        
-        // return the fade colour
-        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
-    }
 }
--- MC2/View/Slide.swift
+++ MC2/View/Slide.swift
@@ -13,14 +13,6 @@ class Slide: UIView {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var mainTitle: UILabel!
     @IBOutlet weak var descLabel: UILabel!
-    @IBOutlet weak var pushButton: UIButton!
-    
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet weak var pushButton: UIButton!    
 
 }
