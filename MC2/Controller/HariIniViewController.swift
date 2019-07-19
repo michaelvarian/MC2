f2fcbcc5dcc8f918873416304ebb6b74ec8bc6b8
Azis Jabbar Susetio <tioazis@gmail.com>
2019-07-20 05:33:44 +0700

    customisasi navbar atas

--- MC2/Controller/HariIniViewController.swift
+++ MC2/Controller/HariIniViewController.swift
@@ -69,6 +69,10 @@ class HariIniViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = false
+    }
+    
     @IBAction func catatTapped(_ sender: UIButton) {
         showActionSheet()
     }
