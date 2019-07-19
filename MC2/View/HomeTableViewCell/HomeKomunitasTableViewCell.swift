ea57e0965b42d9de78de0778df20cbef670f4dce
Clsz <muhammad.reynaldi001@binus.ac.id>
2019-07-19 14:43:51 +0700

    New

--- MC2/View/HomeTableViewCell/HomeKomunitasTableViewCell.swift
+++ MC2/View/HomeTableViewCell/HomeKomunitasTableViewCell.swift
@@ -9,20 +9,21 @@
 import UIKit
 
 class HomeKomunitasTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var imageKomunitas: UIImageView!
     @IBOutlet weak var namaKomunitas: UILabel!
     @IBOutlet weak var deskripsiKomunitas: UILabel!
     
-    override func awakeFromNib() {
+    override
+    func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
